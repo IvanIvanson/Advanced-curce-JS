60cbe8bfdb5c1292e61b3c6a2ab6d223102b9c56
ivan <ghalut@mail.ru>
2022-01-26 21:16:09 +0300

    lesson-2

--- gb_js_shop/src/index.js
+++ gb_js_shop/src/index.js
@@ -1,9 +1,34 @@
 import getProductList from "./mock/data.js";
 import renderGoodsList from "./showcase.js";
-// import './scss/style.scss';
-import  './style.css';
-// import _ from 'lodash';
-import './style.scss';
-const productList = getProductList(20);
+import { send } from './utils.js'
 
-renderGoodsList(productList);
+
+const API_URL = 'http://localhost:3000/api/v1'
+
+let productList = [];
+let cart = [];
+
+send((error) => { console.log(err) }, (res) => { 
+  let list = JSON.parse(res);
+  productList = list;
+  renderGoodsList(productList);
+}, `${API_URL}/catalog`)
+
+
+
+// Пользователь добавляет товар в корзину
+let buyed = {id: 5, title: "new", price: 999};
+send((error) => { console.log(err) }, (res) => {
+  cart.push(buyed)
+}, `${API_URL}/cart`, 'POST', JSON.stringify(buyed), {"Content-Type": "application/json"});
+
+// Пользователь удаляет товар из корзины
+
+function deletJson(url) {
+  return fetch(`${API}${url}`, {
+    method: 'DELETE',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(result => result.json());
+}
